b7a028d9d25b3382fbb8b98740d7c8e516043453
build-africa-eng <afrcanfuture@gmail.com>
2025-05-23 13:16:30 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 export default {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      config: {
-        platform: { env: { DB: 'D1Database' } }
-      }
-    })
+    adapter: adapter()
   }
 };
