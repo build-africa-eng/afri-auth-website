fcd1a73c57d505201f1771e03976ead74110ea7c
build-africa-eng <afrcanfuture@gmail.com>
2025-05-23 11:07:01 +0300

    Create +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { signOut } from '@auth/sveltekit/client';
+  export let data;
+</script>
+
+{#if data.session}
+  <h1>Welcome, {data.session.user.name}</h1>
+  <button on:click={() => signOut({ callbackUrl: '/login' })}>
+    Sign out
+  </button>
+{:else}
+  <h1>Afri Auth</h1>
+  <a href="/login">Login</a>
+{/if}
