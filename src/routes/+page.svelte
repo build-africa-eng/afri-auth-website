cbe527b01f537208b9bf9c1c3417c7df326fc3ee
build-africa-eng <afrcanfuture@gmail.com>
2025-05-23 11:51:36 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,12 @@
 
 {#if data.session}
   <h1>Welcome, {data.session.user.name}</h1>
+  <h2>Users</h2>
+  <ul>
+    {#each data.users as user}
+      <li>{user.email}</li>
+    {/each}
+  </ul>
   <button on:click={() => signOut({ callbackUrl: '/login' })}>
     Sign out
   </button>
