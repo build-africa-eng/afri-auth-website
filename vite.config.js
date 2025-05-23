26b30296f670e8ae0ec6465c1f6eb094481cc0b8
build-africa-eng <afrcanfuture@gmail.com>
2025-05-23 18:26:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
@@ -13,5 +14,10 @@ export default defineConfig({
   },
   ssr: {
     noExternal: ['@auth/sveltekit', '@auth/core']
+  },
+  resolve: {
+    alias: {
+      '@auth/sveltekit/server': path.resolve('node_modules/@auth/sveltekit/dist/server.js')
+    }
   }
 });
