50ea1fee0f2446cce7273e2ac17b98be9115c4c7
build-africa-eng <afrcanfuture@gmail.com>
2025-05-23 11:05:29 +0300

    Create +page.svelte

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { signIn } from '@auth/sveltekit/client';
+</script>
+
+<h1>Afri Auth - Login</h1>
+<button on:click={() => signIn('github', { callbackUrl: '/' })}>
+  Sign in with GitHub
+</button>
