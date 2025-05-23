7d76d5aba425489a474c9931d98b073050397e18
build-africa-eng <afrcanfuture@gmail.com>
2025-05-23 22:40:59 +0300

    Update [...auth].ts

--- src/routes/api/auth/[...auth].ts
+++ src/routes/api/auth/[...auth].ts
@@ -1,17 +1,19 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
 import { D1Adapter } from '@auth/d1-adapter';
 
-export const handle = SvelteKitAuth({
-  providers: [
-    GitHub({
-      clientId: GITHUB_ID,
-      clientSecret: GITHUB_SECRET
-    })
-  ],
-  adapter: D1Adapter({
-    getDB: (platform) => platform.env.DB
-  }),
-  trustHost: true
+export const handle = SvelteKitAuth(async (event) => {
+  return {
+    providers: [
+      GitHub({
+        clientId: event.platform.env.GITHUB_ID,
+        clientSecret: event.platform.env.GITHUB_SECRET
+      })
+    ],
+    adapter: D1Adapter({
+      getDB: () => event.platform.env.DB
+    }),
+    secret: event.platform.env.AUTH_SECRET,
+    trustHost: true
+  };
 });
